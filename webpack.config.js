423a239a0e68b955bd14a8c299238ca5e481ac48
Henok <henatan99@gmail.com>
2021-03-08 16:49:07 +0300

    generates main.js in the dist folder using 'npx webpack' command and tests with a console.log message in the index.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
+    mode: 'none',
+    entry: "./src/index.js",
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),        
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),        
     },
-}
+};
